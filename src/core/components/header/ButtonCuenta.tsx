b06bbbb6fec5f7be108698ef53e4ca484cde9d0b
Pedro Manuel Martinez Pelayo <pedromanuel.martinez@viewnext.com>
2024-11-14 18:23:48 +0100

    Preparado el boton del usuario para recuperar su info

--- src/core/components/header/ButtonCuenta.tsx
+++ src/core/components/header/ButtonCuenta.tsx
@@ -1,12 +1,43 @@
 import Link from "next/link";
 import UserIcon from "../icons/UserIcon";
+import { useUserStore } from "@/features/auth/services/useUserStore";
+import { useEffect, useState } from "react";
+import { getTokens } from "@/features/auth/services/tokenService";
 
 export default function ButtonCuenta() {
+
+    const { user } = useUserStore()
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const tokens = getTokens()
+        if(user || !tokens) return
+
+        (async () => {
+            setLoading(true)
+            
+
+        })()
+
+
+    }, [user])
+
     return (
         <>
             <Link href={'/auth/login'} className="text-lg rounded-lg px-4 py-2 transition-all duration-200 hover:text-onSecondary hover:bg-secondary flex flex-row items-center gap-2">
-                <UserIcon className="w-7 sm:w-8 h-auto" />
-                Mi cuenta
+                {
+                    !user ? (
+                        <>
+                            <UserIcon className="w-7 sm:w-8 h-auto" />
+                            Mi cuenta
+                        </>
+                    )
+                    : (
+                        <>
+                            {user.name}
+                        </>
+                    )
+                }
             </Link>
         </>
     )
--- src/features/auth/services/tokenService.ts
+++ src/features/auth/services/tokenService.ts
@@ -4,3 +4,12 @@ export function saveTokens(tokens: TokenWrapper): void {
     localStorage.setItem('accessToken', tokens.accessToken);
     localStorage.setItem('refreshToken', tokens.refreshToken);
 }
+
+export function getTokens(): TokenWrapper | undefined {
+    const accessToken = localStorage.getItem('accessToken')
+    const refreshToken = localStorage.getItem('accessToken')
+
+    if(!accessToken || !refreshToken) return
+
+    return { accessToken, refreshToken }
+}
