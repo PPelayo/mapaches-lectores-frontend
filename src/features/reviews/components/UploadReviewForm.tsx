15aaef7e7e7c7c88a5e70f22632df8812a306f60
Pedro <pedro.pelayo003@gmail.com>
2024-11-17 01:28:55 +0100

    Agregada funcioanlidad para valorar de manera general un libro

--- src/features/reviews/components/UploadReviewForm.tsx
+++ src/features/reviews/components/UploadReviewForm.tsx
@@ -2,27 +2,30 @@
 
 import { Button, Input } from '@headlessui/react'
 import { FormEvent, useState } from 'react'
+import ReactStars from 'react-stars'
 
 export default function UploadReviewForm() {
-
     const [showButtons, setShowButtons] = useState(false)
     const [comment, setComment] = useState('')
+    const [rating, setRating] = useState(0)
 
     const handleCancel = () => {
         setComment('')
+        setRating(0)
         setShowButtons(false)
     }
 
-    const handleSubmit = (e : FormEvent) => {
+    const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
     }
 
     return (
         <>
             <h2 className="text-xl font-bold">Comentarios</h2>
-            <form 
+            <form
                 onSubmit={handleSubmit}
-                className="grid grid:cols-6 sm:grid-cols-8 w-full my-2 gap-2">
+                className="grid grid:cols-6 sm:grid-cols-8 w-full my-2 sm:gap-2"
+            >
                 <Input
                     value={comment}
                     onChange={(e) => setComment(e.target.value)}
@@ -34,30 +37,40 @@ export default function UploadReviewForm() {
                             `}
                 />
 
-                {
-                    showButtons && (
-                        <>
-                            <Button
-                                type='button'
-                                onClick={handleCancel}
-                                className={
-                                    'col-start-5 sm:col-start-7 text-black rounded-full transition-all duration-200 ease-in data-[hover]:bg-background'
-                                }
-                            >
-                                Cancelar
-                            </Button>
-                            <Button
-                                type='submit'
-                                disabled={!comment}
-                                className={
-                                    'bg-primary rounded-full px-4 py-2 text-md transition-all duration-200 ease-in hover:brightness-75 disabled:opacity-50'
-                                }
-                            >
-                                Guardar
-                            </Button>
-                        </>
-                    )
-                }
+                {showButtons && (
+                    <>
+                        <div className="flex flex-row gap-2 items-center col-span-6 sm:col-span-2">
+                            <span>Valoracion General:</span>
+                            <ReactStars
+                                count={5}
+                                size={24}
+                                color2={'#ffd700'}
+                                value={rating}
+                                half={false}
+                                onChange={(r : number) => setRating(r)}
+                                className="flex "
+                            />
+                        </div>
+                        <Button
+                            type="button"
+                            onClick={handleCancel}
+                            className={
+                                'col-start-1 sm:col-start-7 text-black rounded-full transition-all duration-200 ease-in data-[hover]:bg-background'
+                            }
+                        >
+                            Cancelar
+                        </Button>
+                        <Button
+                            type="submit"
+                            disabled={!comment}
+                            className={
+                                'col-start-6 sm:col-start-8 bg-primary rounded-full px-4 py-2 text-md transition-all duration-200 ease-in data-[hover]:brightness-75 data-[disabled]:opacity-50'
+                            }
+                        >
+                            Guardar
+                        </Button>
+                    </>
+                )}
             </form>
         </>
     )
