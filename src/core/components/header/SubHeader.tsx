1024f8e3ca123dd45db5ce1b49c478fd43c8668e
Pedro <pedro.pelayo003@gmail.com>
2024-11-24 20:30:17 +0100

    fix: cambio button to link en el header

--- src/core/components/header/SubHeader.tsx
+++ src/core/components/header/SubHeader.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function SubHeader() {
     return (
         <section className="bg-secondaryContainer flex justify-center">
             <nav className="w-full max-w-3xl bg-secondaryContainer text-lg flex flex-row items-center justify-between px-0 py-0">
-                <button className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Inicio</button>
-                <button className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Libros</button>
-                <button className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Autores</button>
-                <button className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Editoriales</button>
+                <Link href={'/'} className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Inicio</Link>
+                <Link href={'/books'} className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Libros</Link>
+                <Link href={'/authors'} className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Autores</Link>
+                <Link href={'/publishers'} className="h-full px-4 py-1 rounded-lg transition-colors duration-200 hover:bg-secondary hover:text-onSecondary">Editoriales</Link>
             </nav>
         </section>
     )
