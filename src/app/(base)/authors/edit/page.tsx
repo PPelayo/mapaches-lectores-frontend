8e36aafd05678735effe6cdf77b1e2238d06263f
Pedro <pedro.pelayo003@gmail.com>
2024-12-01 14:36:38 +0100

    fix: Redirigir automaticamente al inicio cuando se crea un publisher o un autor

--- src/app/(base)/authors/edit/page.tsx
+++ src/app/(base)/authors/edit/page.tsx
@@ -1,14 +1,27 @@
+'use client'
+
 import CreateAuthorForm from "@/features/authors/components/CreateAuthorForm";
+import {useRouter} from "next/navigation";
+import toast, {Toaster} from "react-hot-toast";
 
 export default function AuthorsEdit() {
+
+    const router = useRouter()
+
+    const handleCreated = () => {
+        toast.success('Autor creado')
+        router.push('/')
+    }
+
     return (
         <>
+            <Toaster position={'bottom-right'}/>
             <section className="place-self-center max-w-3xl w-full flex flex-col items-center mt-4">
                 <header>
                     <h1 className="text-2xl font-bold">Crear Autor</h1>
                 </header>
                 <main className="w-full h-72 p-4">
-                    <CreateAuthorForm />
+                    <CreateAuthorForm onCreated={handleCreated} />
                 </main>
 
             </section>
--- src/app/(base)/publishers/edit/page.tsx
+++ src/app/(base)/publishers/edit/page.tsx
@@ -1,9 +1,18 @@
+'use client'
 import CreatePublisherForm from "@/features/publishers/components/CreatePublisherForm";
+import {useRouter} from "next/navigation";
+import toast, {Toaster} from "react-hot-toast";
 
 export default function PublishersEdit(){
+    const router = useRouter()
 
+    const handleCreated = () => {
+        router.push('/')
+        toast.success('Editorial creada')
+    }
     return (
         <>
+            <Toaster position={'bottom-right'}/>
             <section className="place-self-center max-w-3xl w-full flex flex-col items-center mt-4">
                 <header>
                     <h1 className="text-2xl font-bold">
@@ -11,7 +20,7 @@ export default function PublishersEdit(){
                     </h1>
                 </header>
                 <main className="w-full h-72 p-4">
-                    <CreatePublisherForm/>
+                    <CreatePublisherForm onCreated={handleCreated}/>
                 </main>
             </section>
         </>
