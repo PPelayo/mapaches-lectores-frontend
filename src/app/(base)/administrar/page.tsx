4b9c3f987682f64e7f26e74c237fdbb2f13dd3ad
Pedro Manuel Martinez Pelayo <pedromanuel.martinez@viewnext.com>
2024-12-10 18:27:27 +0100

    feat: Creada base para ver las requests

--- src/app/(base)/administrar/page.tsx
+++ src/app/(base)/administrar/page.tsx
@@ -1,7 +1,11 @@
+import RequestCreateBookTable from "@/features/requests/components/RequestCreateBookTable";
+
 export default function Administrar() {
     return (
         <>
             <h1>Pagina del administrador</h1>
+
+            <RequestCreateBookTable></RequestCreateBookTable>
         </>
     )
 }
---
+++ src/features/requests/components/RequestCreateBookTable.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+
+export default function RequestCreateBookTable(){
+    return (
+        <>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>FECHA</th>
+                    </tr>
+                    <tr>
+                        <th>USUARIO</th>
+                    </tr>
+                    <tr>
+                        <th>TITULO</th>
+                    </tr>
+                    <tr>
+                        <th>ACCIONES</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>FECHA 1</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>USUAIRO</td>
+                    </tr>
+                    <tr>
+                        <td>TITULO</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </>
+    )
+}
